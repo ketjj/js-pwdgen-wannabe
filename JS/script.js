9052a0642eb46872c93769b37064bbe3960a100b
ketjj <jorjolianiket@gmail.com>
2022-04-05 20:27:35 +0200

    fisrt commit

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+/*
+
+a. chiedere nome, cognome e colore preferito.
+b. stampare 
+
+1. inizializzare/dichiarare le variabili
+2. fare inserire i dati
+3. stampare tutto
+
+*/
+
+const firstName = prompt ("Inserisci il nome");
+const lastName = prompt ("Inserisci il cognome");
+const favoriteColor = prompt ("Il suo colore preferito");
+
+console.log(firstName);
+console.log(lastName);
+console.log(favoriteColor);
+
+
+
+document.getElementById('infoRM').innerHTML = 
+"FIRST NAME: " + firstName + "<br />" +
+"LAST NAME: " + lastName + "<br />" +
+"FAVORITE COLOR: " + favoriteColor;
+
